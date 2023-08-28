43807a3df60dfc5df5ea5c0affa46120275edc79
Hassan Bulega <hassanbulega@gmail.com>
2023-08-28 18:07:36 +0300

    add test to pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,36 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+
+  const pokemons = [
+    {
+      name: 'ivysaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/2/',
+      hiddenAbility: 'chlorophyll',
+    },
+    {
+      name: 'venusaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/3/',
+      hiddenAbility: 'overgrow',
+    },
+  ]
+
+  it('can navigate to a Pokemon\'s details page', function () {
+    cy.visit('http://localhost:5000')
+
+    pokemons.forEach((pokemon) => {
+
+      cy.contains(pokemon.name).click()
+
+      cy.get('div').should('contain', pokemon.name)
+
+
+      if (pokemon.hiddenAbility) {
+        cy.get('.pokemon-ability-name').should('contain', pokemon.hiddenAbility)
+      }
+
+      cy.go('back')
+    })
+  })
 })
