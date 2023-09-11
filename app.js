13911b96b5884ede577454291b895760a4ca6511
Hassan Bulega <hassanbulega@gmail.com>
2023-09-11 12:29:54 +0300

    test failure

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.liste(PORT, () => {
   /* eslint-disable no-console */
   console.log('server started on port 5000')
 })
